6a096f1e0ea0d7b2c91c16cdf57955f77a1a6e73
JustRoys <59421443+JustRoys@users.noreply.github.com>
2024-01-16 04:13:44 +0100

    Update fxmanifest.lua
    version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,6 @@ game "rdr3"
 lua54 "yes"
 
 author 'JustRoy'
-version '1.0'
 description 'This script is made by JustRoy'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
@@ -19,3 +18,9 @@ dependency {
 	"vorp_core",
 	"vorp_inventory",
 }
+
+-- Dont touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/JustRoys/JR-Wash'
