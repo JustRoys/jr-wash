3f55182a4b0bf2b71d537c99150e9bd88c6bc35d
JustRoys <59421443+JustRoys@users.noreply.github.com>
2024-01-16 07:36:30 +0100

    Update config.lua
    removed test things

--- config.lua
+++ config.lua
@@ -2,25 +2,7 @@ Config = {}
 
 Config.Outsiderneeds = true -- set false if you don't use Outsider Needs
 
--- Keys
-Config.Keys = {
-    WashKey = 0x41AC83D1, -- E
-}
-
-Config.Languages = {
-    Wash = "Wash yourself"
-}
-
 Config.WashItems = {
     "washcloth",
     "soap"
-}
-
-Config.UseProps = true
-Config.Distance = 2
-Config.WashProps = {
-    "p_washtub01x",
-    "p_washtub02x",
-    "p_washtub03x",
-    "p_barrel_wash01x"
 }
