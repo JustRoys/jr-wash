930bac6474517da8dbae7bae09405b4638bcb00f
JustRoys <59421443+JustRoys@users.noreply.github.com>
2024-01-16 20:38:37 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,12 @@ game "rdr3"
 lua54 "yes"
 
 author 'JustRoy'
-version '1.1'
-description 'This script is made by JustRoy'
+description 'This script is made by JustRoy and tested in Nieuw-Nederland RP'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+shared_scripts {
+    'config.lua',
+}
 
 client_scripts {
 	'client/client.lua',
