48eab2cdae0b5256c67ed9b623ebb023865be12f
JustRoy <59421443+JustRoys@users.noreply.github.com>
2024-07-16 21:15:05 +0200

    Update fxmanifest.lua
    added version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,11 @@ author 'JustRoy'
 description 'This script is made by JustRoy and tested in Nieuw-Nederland RP'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+version '1.1'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/JustRoys/jr-wash'
+
 shared_scripts {
     'config.lua',
 }
