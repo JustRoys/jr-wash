0188e063497ae852b46c512e3d317060651d2b42
JustRoy <59421443+JustRoys@users.noreply.github.com>
2025-01-24 00:48:42 +0100

    Version 1.3
    - Added props to interact to clean yourself

---
+++ jr-wash/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version "adamant"
+game "rdr3"
+lua54 "yes"
+
+author 'JustRoy'
+description 'This script is made by JustRoy and tested'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+version '1.2'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/JustRoys/jr-wash'
+
+shared_scripts {
+    'shared/language.lua',
+    'config.lua'
+}
+
+client_scripts {
+	'client/client.lua',
+}
+
+server_scripts {
+	'server/server.lua',
+}
+
+dependency {
+	"vorp_core",
+	"vorp_inventory",
+}
---
+++ jr-wash/shared/language.lua
@@ -0,0 +1,14 @@
+Translation = {}
+
+Translation.Langs = {
+    English = {
+        Description = "Durability",
+        Broke = "Your washing item broke",
+        Wash = "Wash",
+    },
+    Dutch = {
+        OpenMenu = "Open Menu",
+        ReviveYourselfCash = "Revive jezelf voor $",
+        Wash = "Wassen",
+    },
+}
